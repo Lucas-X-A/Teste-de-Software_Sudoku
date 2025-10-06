8891a6e578aafff6558d5039dc8dcc0ea1deab2c
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-10-06 16:04:47 -0300

    correção nos comentários dos testes

--- src/test/java/Teste_2.java
+++ src/test/java/Teste_2.java
@@ -18,14 +18,14 @@ public class Teste_2 {
     }
 
     @Test
-    @DisplayName("G2_Sudoku_ok: Deve retornar false para um Sudoku inválido (erro na primeira coluna)")
+    @DisplayName("G2_Sudoku_ok: Deve retornar false para um Sudoku inválido")
     void testeSudokuOkComSolucaoInvalida() {
         G2_Sudoku_ok sudoku = new G2_Sudoku_ok(CAMINHO_INVALIDO);
         assertFalse(sudoku.valido(), "Deveria retornar false para um tabuleiro com erro na primeira coluna.");
     }
 
     @Test
-    @DisplayName("G2_Sudoku_ok: Deve retornar false para um Sudoku inválido (erro na última coluna)")
+    @DisplayName("G2_Sudoku_ok: Deve retornar false para um Sudoku inválido")
     void testeSudokuOkComSolucaoInvalidaUltimaColuna() {
         G2_Sudoku_ok sudoku = new G2_Sudoku_ok(CAMINHO_INVALIDO_2);
         assertFalse(sudoku.valido(), "Deveria retornar false para um tabuleiro com erro na última coluna.");
@@ -42,16 +42,16 @@ public class Teste_2 {
     }
 
     @Test
-    @DisplayName("G2_Sudoku_nok: Deve retornar false para um Sudoku inválido (erro na primeira coluna)")
+    @DisplayName("G2_Sudoku_nok: Deve retornar false para um Sudoku inválido")
     void testeSudokuNokComSolucaoInvalida() {
         G2_Sudoku_nok sudoku = new G2_Sudoku_nok(CAMINHO_INVALIDO);
         assertFalse(sudoku.valido(), "Deveria retornar false, pois o erro na primeira coluna é detectado.");
     }
 
     @Test
-    @DisplayName("G2_Sudoku_nok: [FALHA ESPERADA] Deve retornar true para um Sudoku com erro apenas na última coluna")
+    @DisplayName("G2_Sudoku_nok: Deve retornar true para um Sudoku com erro apenas na última coluna")
     void testeSudokuNokComErroNaoDetectado() {
         G2_Sudoku_nok sudoku = new G2_Sudoku_nok(CAMINHO_INVALIDO_2);
-        assertFalse(sudoku.valido(), "Este teste deve falhar para demonstrar o bug na validação da última coluna.");
+        assertFalse(sudoku.valido(), "Este teste deve falhar.");
     }
 }
